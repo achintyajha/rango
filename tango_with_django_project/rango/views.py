a42dbedd1355ba031986b3b822935687bd58ba20
achintyajha <achintyakjha@gmail.com>
2021-08-02 09:34:23 +0530

    framework-pages

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
-from rango.models import Category, Page
+from rango.models import Category
+from rango.models import Page
+
 
 def index(request):
     # Query the database for a list of ALL categories currently stored.
@@ -10,12 +12,10 @@ def index(request):
     # Retrieve the top 5 only - or all if less than 5.
     # Place the list in our context_dict dictionary
     # that will be passed to the template engine.
-
-    category_list = Category.objects.order_by('-likes')[:5] # - for descending
-    # dictionary for template variables
+    category_list = Category.objects.order_by('-likes')[:5]
     context_dict = {'categories': category_list}
 
-    return render(request, 'rango/index.html', context=context_dict)
+    return render(request, 'rango/index.html', context_dict)
 
 
 def about(request):
@@ -23,7 +23,7 @@ def about(request):
     context_dict = {'boldmessage': "Hello, this is the most useless page ever"}
     return render(request, 'rango/about.html', context=context_dict)
 
-def show_category(request, category_name_url):
+def show_category(request, category_name_slug):
     # Create a context dictionary which we can pass
     # to the template rendering engine.
     context_dict = {}
@@ -49,4 +49,5 @@ def show_category(request, category_name_url):
         context_dict['category'] = None
         context_dict['pages'] = None
         # Go render the response and return it to the client.
-        return render(request, 'rango/category.html', context_dict)
+    
+    return render(request, 'rango/category.html', context_dict)
